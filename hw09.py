86d11c3abce4af2fbd16835c38346fcc5fd76963
OleksaSolo <9095945@gmail.com>
2023-07-05 12:17:16 +0300

    It's work

--- hw09.py
+++ hw09.py
@@ -3,60 +3,102 @@ CONTACTS = {}
 def input_error(func):
     def inner(*args):
         try:
-            result = add(*args)
+            return func(*args)
         except KeyError:
-            return f"Enter Key"
-        print(result)
-        if result == "KeyError":
-            return f"Enter Key"
-        else:
-            return result
+            return "Enter Key"
+        except IndexError as e:
+            return "You are entering an invalid command."
     return inner    
-    # if func == "add":
-    #     return f"Enter Key"
-    # if text_error == "ValueError":
-    #     return f"Enter Value"
-    # if text_error == "IndexError":
-    #     return f"Enter Index"
-    # else:
-    #     return f"All OK."
     
 @input_error
 def add(*args):
     name = args[0]
     phone = args[1]
-    print(CONTACTS)
+    #print(CONTACTS)
     CONTACTS[name] = phone
     print(CONTACTS)
     return f"Add success {name} {phone}"
 
+def hello(*args):
+    return f"How can I help you?"
+
+@input_error
+def change(*args):
+    name = args[0]
+    phone = args[1]
+    #print(CONTACTS)
+    if CONTACTS.get(name) != None:
+        CONTACTS.pop(name)
+        CONTACTS[name] = phone
+        #print(CONTACTS)
+        return f"Update success {name} {phone}"
+    else:
+        return f"Notebook have not {name}"
+
+@input_error
+def phone(*args):
+    name = args[0]
+    #print(CONTACTS)
+    if CONTACTS.get(name) != None:
+        #CONTACTS.pop(name)
+        phone = CONTACTS[name]
+        #print(CONTACTS)
+        return f"{name} have phone number: {phone}"
+    else:
+        return f"Notebook have not {name}"
+
+def show_all(*args):
+    result = "\nNotebook \n"
+    if len(CONTACTS) >0:
+        for key, value in CONTACTS.items():
+            result += "name: " + key + "  phone: " + value + "\n"
+    else:
+        result += "is blank"
+    return result
+
 def end(*args):
     return f"Good bye!"
 
-
 def no_command(*args):
-    return "Unknown command"
+    return "Unknown command. I don't understand. Please, input rigth command."
 
 
 def parser(text: str) -> tuple[callable, tuple[str]|None]:
-    #print(text.startswith(text))
-    if text.startswith("add"):
-        return add, text.replace("add", "").strip().split()
-    elif text.startswith("exit") or text.startswith("close") or text.startswith("good bye"):
+    input_start = text.strip().split()
+    input_command = input_start[0].lower()
+    #print(input_start[0].lower())
+    if input_command == "good" or input_command == "show":
+        if len(input_start) > 1:
+            input_command += " " + input_start[1].lower()
+    #print(input_command)
+    if input_command == "add":
+        return add, text.replace(input_start[0], "").strip().split()
+    elif input_command == "hello":
+        return hello, ""
+    elif input_command == "change":
+        return change, text.replace(input_start[0], "").strip().split()
+    elif input_command == "phone":
+        return phone, text.replace(input_start[0], "").strip().split()
+    elif input_command == "show all":
+        return show_all, ""
+    elif input_command == "exit" or input_command == "close" or input_command == "good bye":
         #return end, text.replace("end", "").strip().split()
         return end, ""
-    return no_command, None
+    return no_command, ""
     
 
 def main():
+    print("Input command: hello, add name phone, change name phone, phone name, show all, good bye/close/exit")
     while True:
-        user_input = input("Input command: hello, add name phone, change name phone, phone name, show all, good bye/close/exit>>>")
+        user_input = input(">>>")
         command, data = parser(user_input)
         result = command(*data)
         print(result)
-        if result == "Good bye!":
+        if result != "Good bye!":
+            print("\nInput command: hello, add name phone, change name phone, phone name, show all, good bye/close/exit")
+        else:
             break
     
 if __name__ == "__main__":
     main()
-
+    
